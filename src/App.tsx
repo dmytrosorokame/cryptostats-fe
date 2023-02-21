91e5d232c31dff66376cc60242b0983217eea263
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-21 13:08:42 +0200

    init & create first component

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import LoginForm from './components/auth/LoginForm/LoginForm';
+
+function App() {
+  return <LoginForm />
+}
+
+export default App;
---
+++ src/components/auth/LoginForm/LoginForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <div>LoginFor</div>
+  )
+}
+
+export default LoginForm
