ee21195054244570cdb038e85b847f6cd39b249a
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-21 13:43:51 +0200

    change tailwind settings

--- src/components/auth/LoginForm/LoginForm.tsx
+++ src/components/auth/LoginForm/LoginForm.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Button, TextField } from "@mui/material";
+import React from "react";
 
 const LoginForm = () => {
   return (
-    <div>LoginFor</div>
-  )
-}
+    <div className="flex justify-center items-center flex-col h-screen gap-8">
+      <h1 className="text-6xl">Cryptostats</h1>
+      <div className="flex flex-col gap-2">
+        <TextField label="Email" className="w-80" type="email" required />
+        <TextField label="Password" className="w-80" type="password" required />
+      </div>
+      <Button variant="contained" className="w-80">
+        <span className="p-1">Login</span>
+      </Button>
+    </div>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: "class", // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      sans: ["Roboto"],
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
