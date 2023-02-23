c35c810503867f23dd4e75a6c4b924a2826407de
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-02-23 08:37:48 +0200

    add base home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 const HomePage: React.FC = () => {
-  return <div>HomePage</div>;
+  const handleConnectCoinbase = () => {
+    if (process.env.REACT_APP_COINBASE_AUTH_URL) {
+      window.location.href = process.env.REACT_APP_COINBASE_AUTH_URL;
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center flex-col h-screen">
+      <Button variant="contained" size="large" onClick={handleConnectCoinbase}>
+        Connect Coinbase
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
